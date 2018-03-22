da598965f8cbd0355e4dceece4396c8dc2285f43
cdmccauley <cdmccauley@gmail.com>
2018-03-21 19:51:21 -0600

    ch01 pursue complete

---
+++ ch01-pursue/ch01-pursue-pt2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Chapter 1, Pursue, Part 2</title>
+</head>
+<body>
+<?php
+
+# ch01-pursue-pt2.php
+# christohper mccauley, chris@mccauley.tech
+# 21-03-2018
+
+$firstName = 'Chris';
+$lastName = 'McCauley';
+$courseName = 'PHP MySQL';
+$assignmentName = 'Pursue';
+
+print '<b>ITEC-2235:</b> ' . $courseName . '<br><b>Chapter 1:</b> ' . $assignmentName . '<br><b>Author:</b> ' . $firstName . ' ' . $lastName;
+
+?>
+</body>
+</html>
---
+++ ch01-pursue/ch01-pursue-pt3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Chapter 1, Pursue, Part 3</title>
+</head>
+<body>
+<?php
+
+# ch01-pursue-pt3.php
+# christohper mccauley, chris@mccauley.tech
+# 21-03-2018
+
+$studentID = 8408675309;
+$studentStartYear = 2017;
+$studentAge = 32.78;
+
+print "Student number $studentID is $studentAge years old and started the Davis Tech IT program in $studentStartYear";
+
+?>
+</body>
+</html>
