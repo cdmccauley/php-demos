a25e727c238f165db75e79c288df3613289277f3
cdmccauley <cdmccauley@gmail.com>
2018-04-17 13:48:50 -0600

    ch12-exercises complete

--- ch12-exercises/12.12-12.13/loggedin.php
+++ ch12-exercises/12.12-12.13/loggedin.php
@@ -1,8 +1,11 @@
-<?php # Script 12.4 - loggedin.php
+<?php # Script 12.13 - loggedin.php #3
 // The user is redirected here from login.php.
 
-// If no cookie is present, redirect the user:
-if (!isset($_COOKIE['user_id'])) {
+session_start(); // Start the session.
+
+// If no session value is present, redirect the user:
+// Also validate the HTTP_USER_AGENT!
+if (!isset($_SESSION['agent']) OR ($_SESSION['agent'] != md5($_SERVER['HTTP_USER_AGENT']) )) {
 
 	// Need the functions:
 	require ('includes/login_functions.inc.php');
@@ -16,7 +19,7 @@ include ('includes/header.html');
 
 // Print a customized message:
 echo "<h1>Logged In!</h1>
-<p>You are now logged in, {$_COOKIE['first_name']}!</p>
+<p>You are now logged in, {$_SESSION['first_name']}!</p>
 <p><a href=\"logout.php\">Logout</a></p>";
 
 include ('includes/footer.html');
