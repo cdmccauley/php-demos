2f5ef45390c449d25b3c54c6ba629bbd7efb12f3
cdmccauley <cdmccauley@gmail.com>
2018-04-22 18:25:18 -0600

    completed 14.1 and 14.2 exercises

--- ch13-pursue/PacificTrails.php
+++ ch13-pursue/PacificTrails.php
@@ -7,19 +7,43 @@
 <body>
 <?php
 
-$email = $_POST['myEmail'];
-$nights = $_POST['myNights'];
+// spam_scrubber() declaration
+function spam_scrubber($value) {
+    $very_bad = array('to:', 'cc:', 'bcc:', 'content-type:', 'mime-version:', 'multipart-mixed:', 'content-transfer-encoding:');
+
+    foreach ($very_bad as $v) {
+        if (stripos($value, $v) !== false) return '';
+    }
+
+    $value = str_replace(array( "\r", "\n", "%0a", "%0d"), ' ', $value);
+
+    return trim($value);
+}
+
+// use spam_scrubber()
+$scrubbed = array_map('spam_scrubber', $_POST);
+
+// var declarations
+$email = $scrubbed['myEmail'];
+$nights = $scrubbed['myNights'];
 $errors = array();
 
 // validate nights
-if ($nights < 0 || $nights > 31) { 
+if ($nights < 0 || $nights > 31 || empty($nights)) { 
     $errors[] = "Nights must be 1-31";
 }
+
+// using to demonstrate spam_scrubber
+if (!empty($_POST['myEmail']) && empty($email)) {
+    $errors[] = "Manipulation detected";
+}
+
 // validate email
 if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
     $errors[] = "Invalid e-mail address";
 }
-// if there are errors, print them
+
+// echo output
 if (!(empty($errors))) {
     foreach ($errors as $error) {
         echo ($error . '<br />');
